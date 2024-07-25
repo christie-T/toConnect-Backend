8e1b6810d960d848b9515b51f0e056175a91b2fa
Christie Tang <christietang23@gmail.com>
2024-07-25 04:53:48 -0400

    added frontend files

---
+++ frontend/reportHide.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { Alert } from 'react-native';
+import { BASE_URL } from './server_config.js';
+
+// Reporting and hiding posts for explore and home page
+export async function addReport(selectedPostId: string | null, reason: string) {
+  console.log('es!')
+
+    try {
+        if (!selectedPostId) throw new Error('No post selected for reporting');
+        
+        // Make a POST request to your Django backend
+        const response = await axios.post(`${BASE_URL}/add_report`, {
+            selectedPostId: selectedPostId,
+            reason: reason
+        }, {
+            withCredentials: true // Include this if you need to send cookies with the request
+        });
+
+        if (response.status === 200) {
+            Alert.alert('Report submitted', 'Thank you for your contribution to help keep our platform safe!');
+        } else {
+            throw new Error(response.data.error || 'An error occurred while submitting the report');
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            Alert.alert('Error', error.message);
+        }
+    }
+}
+
+export async function handleHide(auth:any, postId: string) {
+    
+    if (!auth.session) return; // Return early if no active user session
+
+    try {
+        // Make a POST request to your Django backend
+        const response = await axios.post(`${BASE_URL}/handle_hide`, {
+            postId: postId
+        }, {
+            withCredentials: true // Include this if you need to send cookies with the request 
+          }); 
+
+        if (response.status === 200) {
+            Alert.alert('Post successfully blocked');
+        } else {
+            throw new Error(response.data.error || 'An error occurred while blocking the post');
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            Alert.alert('Error', error.message);
+        }
+    }
+}
